cd285eeaa0423c74fe02e77f7781a81ae59c748a
Dan Mills <evilhamsterman@gmail.com>
2024-06-12 03:45:34 +0000

    Fix tests

--- src/sops/install.sh
+++ src/sops/install.sh
@@ -8,13 +8,13 @@ set -e
 # `ensure_nanolayer` is a bash function that will find any existing nanolayer installations, 
 # and if missing - will download a temporary copy that automatically get deleted at the end 
 # of the script
-ensure_nanolayer nanolayer_location "v0.5.4"
+ensure_nanolayer nanolayer_location "v0.5.6"
 
 
 $nanolayer_location \
     install \
     devcontainer-feature \
-    "ghcr.io/devcontainers-contrib/features/gh-release:1.0.23" \
+    "ghcr.io/devcontainers-contrib/features/gh-release:1.0.25" \
     --option repo='mozilla/sops' --option binaryNames='sops' --option version="$VERSION"
     
 
